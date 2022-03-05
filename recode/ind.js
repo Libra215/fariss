6d5e0f8dfdce49f9eab206e053350d7f455c8412
Libra215 <librazxdt11@gmail.com>
2022-03-05 23:04:00 +0700

    neb

---
+++ recode/ind.js
@@ -0,0 +1,234 @@
+exports.paymentstore = (owner) => {
+return `*PAYMENT STORE*
+Dana : 0857-3511-5194
+Ovo : 0857-3511-5194
+Pulsa : None
+Gopay : 0857-3511-5194
+ShopeePay : 0857-3511-5194
+
+Note :
+Jika Sudah Transfer Wajib Kirim
+Bukti Transfer Ke Nomor Di Bawah
+Agar Bisa Cepat Di Proses Langsung
+
+Wa.me/${owner}
+`
+}
+
+exports.diamondFF = () => {
+return `*DROP DATA DIAMOND FF*
+• VIA ID
+• LEGAL 100%
+• PROSES 1-7 MENIT
+*25 💎 Rp 4.000*
+*50 💎 Rp 7.000*
+*70 💎 Rp 10.000*
+*100 💎 Rp 15.000*
+*140 💎 Rp 20.000*
+*210 💎 Rp 30.000*
+*355 💎 Rp 50.000*
+*500 💎 Rp 70.000*
+*720 💎 Rp 98.000*
+*1075 💎 Rp 145.000*
+*1440 💎 Rp 193.000*
+*2000 💎 Rp 265.000*
+
+*Member Mingguan Rp 30.000*
+*Member Bulanan Rp 150.000*
+
+Note : 
+• Harga sewaktu waktu berubah
+• Pastikan ID Sudah benar
+• Pemesanan lebih tinggal x kan saja
+• Kesalahan Pengirim data tidak ada reff
+`
+}
+
+exports.hagostore = () => {
+return `*MAAF KAMI SUDAH TIDAK MELAYANI TOP UP HAGO LAGI*
+
+
+`
+}
+
+exports.chipstore = () => {
+return `Note : 
+• Harga sewaktu waktu berubah
+• Pastikan ID & Server Sudah benar
+• Pemesanan lebih tinggal x kan saja
+• Kesalahan Pengirim data tidak ada reff
+
+*DROP DATA CHIP*
+• VIA ID + NICK
+• LEGAL 100%
+• PROSES 1-5 MENIT
+• STOK UNLIMITED
+
+*30M Koin Emas-D Rp 6.000*
+*60M Koin Emas-D Rp 12.000*
+*90M Koin Emas-D Rp 18.000*
+*100M Koin Emas-D Rp 19.500*
+*200M Koin Emas-D Rp 31.000*
+*400M Koin Emas-D Rp 62.000*
+*430M Koin Emas-D Rp 68.000*
+*500M Koin Emas-D Rp 81.500*
+*2B Koin Emas-D Rp 251.000*
+*4B Koin Emas-D Rp 502.000*
+*6B Koin Emas-D Rp 753.000*
+*8B Koin Emas-D Rp 1.004.000*
+*10B Koin Emas-D Rp 1.255.000*
+*12B Koin Emas-D Rp 1.506.000*
+*14B Koin Emas-D Rp 1.757.000*
+
+Note : 
+• Harga sewaktu waktu berubah
+• Pastikan ID Sudah benar
+• Pemesanan lebih tinggal x kan saja
+• Kesalahan Pengirim data tidak ada reff
+`
+}
+
+exports.diamondML = () => {
+return `*DROP DATA DIAMOND ML*
+• VIA ID & SERVER
+• LEGAL 100%
+• PROSES 1-5 MENIT
+
+*86 💎 Rp 20.000*
+*172 💎 Rp 40.000*
+*257 💎 Rp 60.000*
+*344 💎 Rp 80.000*
+*429 💎 Rp 97.500*
+*514 💎 Rp 116.000*
+*600 💎 Rp 135.400*
+*706 💎 Rp 156.614*
+*878 💎 Rp 194.282*
+*963 💎 Rp 213.897*
+*1412 💎 Rp 310.756*
+*2195 💎 Rp 455.170*
+*3688 💎 Rp 760.000*
+*4394 💎 Rp 890.982*
+*5532 💎 Rp 1.145.000*
+*9288 💎 Rp 1.901.000*
+
+*Starlight Membership Bonus 12 💎 Rp 97.000*
+*Twilight Pass [PROMO ]  Rp 97.000*
+*Starlight Membership  Rp 127.000*
+*Twilight  Rp 127.000*
+*Starlight Membership Plus [PROMO ] Rp 200.000*
+*Mobile Legend Member Plus Rp 285.000*
+`
+}
+
+exports.garenashell = () => {
+return `*DROP DATA GARENA SHELL*
+• VIA ID
+• LEGAL 100%
+• PROSES 1-5 MENIT
+• STOK UNLIMITED
+
+*33 SHELL 💰	Rp 10.000*
+*66 SHELL 💰	Rp 20.000*
+*99 SHELL 💰	Rp 30.000*
+*165 SHELL 💰	Rp 50.000*
+*330 SHELL 💰	Rp 100.000*
+
+Note : 
+• Harga sewaktu waktu berubah
+• Pastikan ID Sudah benar
+• Pemesanan lebih tinggal x kan saja
+• Kesalahan Pengirim data tidak ada reff
+`
+}
+
+exports.cbcashstore = () => {
+return `*DROP DATA POINT BLANK*
+• VIA ID
+• LEGAL 100%
+• PROSES 1-5 MENIT
+• STOK UNLIMITED
+
+*1200 PB CASH 💸	Rp 10.000*
+*2400 PB CASH 💸	Rp 20.000*
+*6000 PB CASH 💸	Rp 50.000*
+*12000 PB CASH 💸	Rp 95.000*
+*24000 PB CASH 💸	Rp 200.000*
+*36000 PB CASH 💸	Rp 280.000*
+*60000 PB CASH 💸	Rp 480.000*
+Note : 
+• Harga sewaktu waktu berubah
+• Pastikan ID Sudah benar
+• Pemesanan lebih tinggal x kan saja
+• Kesalahan Pengirim data tidak ada reff
+`
+}
+
+exports.codmstore = () => {
+return `*DROP DATA CALL OF DUTY*
+• VIA ID
+• LEGAL 100%
+• PROSES 1-5 MENIT
+• STOK UNLIMITED
+
+*31 CP 💸	Rp 5.000*
+*62 CP 💸	Rp 10.000*
+*127 CP 💸	Rp 20.000*
+*317 CP 💸  Rp 50.000*
+*634 CP 💸  Rp 90.000*
+*1373 CP 💸 Rp 195.000*
+*1901 CP 💸 Rp 250.000*
+*3300 CP 💸 Rp 395.000*
+
+Note : 
+• Harga sewaktu waktu berubah
+• Pastikan ID Sudah benar
+• Pemesanan lebih tinggal x kan saja
+• Kesalahan Pengirim data tidak ada reff
+`
+}
+
+exports.pubgstore = () => {
+return `*DROP DATA UC PUBG*
+• VIA ID
+• LEGAL 100%
+• PROSES 1-5 MENIT
+• STOK UNLIMITED
+
+*53 💸	Rp 10.000*
+*105 💸	Rp 22.000*
+*131 💸	Rp 29.000*
+*263 💸	Rp 48.500*
+*530 💸	Rp 96.000*
+*825 💸	Rp 143.000*
+*1100 💸Rp 187.000*
+*1925 💸Rp 319.430*
+*2200 💸Rp 372.680*
+*2463 💸 Rp 422.230*
+*2730 💸 Rp 456.180*
+*3025 💸 Rp 502.205*
+*3300 💸 Rp 552.205*
+*3563 💸 Rp 602.680*
+*3830 💸 Rp 632.180*
+*4125 💸 Rp 687.180*
+*4400 💸 Rp 732.180*
+*4663 💸 Rp 776.000*
+
+Note : 
+• Harga sewaktu waktu berubah
+• Pastikan ID Sudah benar
+• Pemesanan lebih tinggal x kan saja
+• Kesalahan Pengirim data tidak ada reff
+`
+}
+exports.donasi = () => { 
+return`*HAI KAK MAU BENERAN DONASI ATAU TIDAK KAK*
+*SEMOGA TIDAK DILIHAT DOANG*
+
+*PAYMENT NOT*
+DANA: (0858-1248-6745)
+OVO: (0858-1248-6745)
+PULSA: (0858-1248-6745)
+
+*TERIMA KASIH KAK*
+*LOVE YOU😋*`
+}
